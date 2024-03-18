03196e2f0b24cea78acbfa751d8ee104d3a851d0
Oz_Lin <lin@ozlin.info>
2024-03-18 17:44:28 +1100

    form fields fixup

--- src/course_listing/courses/forms.py
+++ src/course_listing/courses/forms.py
@@ -4,4 +4,5 @@ from .models import Course
 class CourseForm(forms.ModelForm):
     class Meta:
         model = Course
-        fields = ['title', 'instructor', 'duration', 'description']
+        fields = ['user', 'name', 'date', 'location', 'description', 'photo', 'cost',
+                  'places_available', 'learning_outcomes']
